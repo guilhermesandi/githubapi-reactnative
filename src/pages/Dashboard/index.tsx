e7713372fc63cee4bfee6db97ec67a17d6fd31d4
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-03-15 19:50:42 -0300

    Repositories Style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Repositories, Avatar } from './styles';
 
 import Navbar from '../../components/Navbar';
+import { Text, View } from 'react-native';
 
 const Dashboard: React.FC = () => {
   return (
     <>
       <Navbar />
-      <Container>Hello World!</Container>
+
+      <Container>
+        <Repositories>
+          <Avatar
+            source={{
+              uri: 'https://avatars.githubusercontent.com/u/38008649?v=4',
+            }}
+          />
+          <View>
+            <Text>Login</Text>
+            <Text>Quantidade de seguindores</Text>
+            <Text>Quantidade de Seguindo</Text>
+          </View>
+        </Repositories>
+      </Container>
     </>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,3 +1,12 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.Text``;
+export const Container = styled.Text`
+  display: flex;
+`;
+
+export const Repositories = styled.Text``;
+
+export const Avatar = styled.Image`
+  width: 100px;
+  height: 100px;
+`;
