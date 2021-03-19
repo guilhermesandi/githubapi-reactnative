b71dcb275dab08a25bd7b4d4d50521b07d23d45e
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-03-18 23:47:13 -0300

    Update index.tsx

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -9,37 +9,35 @@ import { UsersContext } from '../../context/UsersContext';
 const Dashboard: React.FC = () => {
   const { users, searchLogin } = useContext(UsersContext);
 
+  const filteredData = users.filter(user => {
+    if (searchLogin === '') {
+      return user;
+    } else if (user.login.toLowerCase().includes(searchLogin.toLowerCase())) {
+      return user;
+    }
+  });
+
   return (
     <>
       <Navbar />
 
       <ScrollView>
         <Container>
-          {users
-            .filter(user => {
-              if (searchLogin === '') {
-                return user;
-              } else if (
-                user.login.toLowerCase().includes(searchLogin.toLowerCase())
-              ) {
-                return user;
-              }
-            })
-            .map(user => (
-              <User key={user.id}>
-                <Avatar
-                  source={{
-                    uri: user.avatar_url,
-                  }}
-                />
-                <UserInfo>
-                  <Login>{user.login}</Login>
-                  <Info>
-                    followers {user.followers} | following {user.following}
-                  </Info>
-                </UserInfo>
-              </User>
-            ))}
+          {filteredData.map(user => (
+            <User key={user.id}>
+              <Avatar
+                source={{
+                  uri: user.avatar_url,
+                }}
+              />
+              <UserInfo>
+                <Login>{user.login}</Login>
+                <Info>
+                  followers {user.followers} | following {user.following}
+                </Info>
+              </UserInfo>
+            </User>
+          ))}
         </Container>
       </ScrollView>
     </>
